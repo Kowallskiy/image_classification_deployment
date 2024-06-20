3e7cb7b2ed5379bddba92fb587bfa694568fdfc4
unknown <trane-101@yandex.ru>
2024-06-20 15:45:18 +0400

    fixed a bug related to converting an image into tensor

--- streamlit.py
+++ streamlit.py
@@ -26,7 +26,7 @@ def load_model(path):
 def transform_image(image, device):
     transform = A.Compose([
         A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ToTensorV2
+        ToTensorV2()
     ])
     image = transform(image=np.array(image))['image']
     image_tensor = torch.tensor(image).unsqueeze(0).to(device)
