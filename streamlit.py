099b8a9f9fbb4748daebb076d6b5b4eb5fed7e77
unknown <trane-101@yandex.ru>
2024-06-22 22:02:29 +0400

    modified the layout

--- streamlit.py
+++ streamlit.py
@@ -22,7 +22,7 @@ class_idx_to_name_dict = {
     7: "Pythium_Fruit_Rot_Fungi"
 }
 
-# Stores general recommendations in case the model decided an image is out-of-distribution
+# Stores general recommendations in case the model decides an image is out-of-distribution
 general_recommendations = """
 - **Soil Preparation**: Ensure well-drained, fertile soil with a pH of 6.0 to 6.8.
 - **Watering**: Water cucumbers consistently, aiming for 1-2 inches of water per week. Use drip irrigation or soaker hoses to keep foliage dry and reduce disease risk.
@@ -34,43 +34,43 @@ general_recommendations = """
 """
 
 def load_model(path: str) -> torch.nn.Module:
-    '''
-    Load a trained Pytorch model from a specified file path.
+    """
+    Load a PyTorch model from the specified file path.
 
     Args:
-        path (str): The file path to saved model.
+        path (str): The file path to the saved model.
 
     Returns:
-        torch.nn.Module: The loaded Pytorch model
-    '''
+        torch.nn.Module: The loaded PyTorch model.
+    """
     model = torch.load(path)
     model.eval()
     return model
 
 def load_recommendations(path: str) -> pd.DataFrame:
-    '''
-    Load a csv file containing recommendations from a specified file path.
+    """
+    Load recommendations from a CSV file.
 
     Args:
-        path (str): The file path to the recommendations.
+        path (str): The file path to the CSV containing recommendations.
 
     Returns:
-        pd.DataFrame: The loaded recommendations.
-    '''
+        pd.DataFrame: DataFrame containing the recommendations.
+    """
     recommendations = pd.read_csv(path)
     return recommendations
 
 def transform_image(image: Image.Image, device: torch.device) -> torch.Tensor:
-    '''
+    """
     Transform an image for model inference.
 
     Args:
         image (Image.Image): The input image to be transformed.
-        device (torch.device): The device to which image tensor will be moved.
-    
+        device (torch.device): The device to which the image tensor will be moved.
+
     Returns:
-        torch.Tensor: Transformed image tensor.
-    '''
+        torch.Tensor: The transformed image tensor.
+    """
     transform = A.Compose([
         A.Resize(224, 224),
         A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -81,44 +81,43 @@ def transform_image(image: Image.Image, device: torch.device) -> torch.Tensor:
     return image_tensor
 
 def calculate_probability_and_predicted_class(output: torch.Tensor) -> tuple:
-    '''
-    Calculate probability and predicted class from the model.
+    """
+    Calculate the probability and predicted class from model output.
 
     Args:
-        output (torch.Tensor): The model's output tensor.
+        output (torch.Tensor): The output tensor from the model.
 
     Returns:
-        A tuple, containing probability and preicted class.
-    '''
+        tuple: A tuple containing the probability and predicted class.
+    """
     p = torch.softmax(output, dim=1)
     probability, predicted_class = torch.max(p, 1)
     return probability, predicted_class
 
 def calculate_energy(output: torch.Tensor) -> torch.Tensor:
-    '''
-    Calculate the energy of the model's output for out-of-distribution detection.
+    """
+    Calculate the energy of the model output.
 
     Args:
-        output (torch.Tensor): The model's output tensor.
+        output (torch.Tensor): The output tensor from the model.
 
     Returns:
-        torch.Tensor: The energy of the current output.
-    '''
+        torch.Tensor: The calculated energy.
+    """
     return -torch.logsumexp(output, dim=1)
 
 def inference(model: torch.nn.Module, image: Image.Image, device: torch.device) -> dict:
-    '''
-    Perfomrs inference on an input image using the specified model.
+    """
+    Perform inference on an input image using the specified model.
 
     Args:
-        model (torch.nn.Module): The trained Pytorch model for making predictions.
+        model (torch.nn.Module): The loaded PyTorch model.
         image (Image.Image): The input image for inference.
         device (torch.device): The device for computation.
 
     Returns:
-        The dictionary containing the inference result.
-    '''
-
+        dict: A dictionary containing the inference result.
+    """
     image_tensor = transform_image(image, device)
 
     with torch.inference_mode():
@@ -128,8 +127,8 @@ def inference(model: torch.nn.Module, image: Image.Image, device: torch.device) 
         probability, predicted_class = calculate_probability_and_predicted_class(output)
         probability_value = probability.item()
         predicted_class_value = predicted_class.item()
-        if energy_value > ENERGY_THRESHOLD:            
-            
+
+        if energy_value > ENERGY_THRESHOLD:
             return {
                 'result': 'unknown',
                 'energy': energy_value,
@@ -142,14 +141,14 @@ def inference(model: torch.nn.Module, image: Image.Image, device: torch.device) 
             'probability': probability_value,
             'predicted_class': predicted_class_value
         }
-    
+
 def classify_image(image: Image.Image):
-    '''
-    Classify the input image and display the results using Streamlit.
+    """
+    Classify an input image and display the results using Streamlit.
 
     Args:
-        image (Image.Image): The image to be classified.
-    '''
+        image (Image.Image): The input image for classification.
+    """
     model_path = "resnet50_cucumber.pth"
     model = load_model(path=model_path)
     device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
@@ -159,30 +158,35 @@ def classify_image(image: Image.Image):
     recommendations = load_recommendations(rec_path)
 
     if result['result'] == 'unknown':
-        st.write("The class is of unknown origin")
-        st.write(f"The energy is {result['energy']:.4f}, but the energy threshold is {ENERGY_THRESHOLD:.4f}")
-        st.write(f"Potentially it could be: {class_idx_to_name_dict[result['predicted_class']]} with the probability {result['probability']:.4f}")
-        st.write("General Recommendations for Cucumber:")
+        st.write("### The class is of unknown origin")
+        st.write(f"**Energy**: {result['energy']:.4f}, but the energy threshold is {ENERGY_THRESHOLD:.4f}")
+        st.write(f"**Potential class**: {class_idx_to_name_dict[result['predicted_class']]} with the probability {result['probability']:.4f}")
+        st.write("### General Recommendations for Cucumber:")
         st.write(general_recommendations)
     else:
-        st.write(f"Probability: {result['probability']:.4f}")
-        st.write(f"Predicted class: {class_idx_to_name_dict[result['predicted_class']]}")
+        st.write(f"### Probability: {result['probability']:.4f}")
+        st.write(f"### Predicted class: {class_idx_to_name_dict[result['predicted_class']]}")
         if class_idx_to_name_dict[result['predicted_class']] not in ["Healthy_Crop_Cucumber", "Healthy_Crop_Leaf"]:
-            st.write("Recommendations")
-            st.write("Pesticide Methods:")
-            # This code can be read like this:
-            # recommendations['disease_name' == 'predicted_disease_name']['give_pesticide_recom']
+            st.write("### Recommendations")
+            st.write("**Pesticide Methods:**")
             st.markdown(recommendations[recommendations['disease'] == class_idx_to_name_dict[result['predicted_class']]]['pesticide'].values[0])
-            st.write("Non-pesticide Methods:")
+            st.write("**Non-pesticide Methods:**")
             st.markdown(recommendations[recommendations['disease'] == class_idx_to_name_dict[result['predicted_class']]]['non-pesticide'].values[0])
         else:
             st.markdown(recommendations[recommendations['disease'] == class_idx_to_name_dict[result['predicted_class']]]['maintenance'].values[0])
 
 def main():
-    '''
-    Main function to run the Streamlit app for cucumber dataset.
-    '''
+    """
+    Main function to run the Streamlit app for cucumber disease detection.
+    """
     st.title("Disease Detection for Cucumber")
+    
+    gif_url = "https://i.gifer.com/embedded/download/edP.gif"
+    st.markdown(
+        f'<img src="{gif_url}" width="600" alt="Cucumber Disease Detection"/>',
+        unsafe_allow_html=True
+    )
+    
     st.write("Upload your image of cucumber or choose one of the example images below:")
 
     example_images = os.listdir("examples")
@@ -191,15 +195,14 @@ def main():
     if selected_example != 'None':
         image = Image.open(selected_example)
         st.image(image, caption="Selected Example Image", use_column_width=True)
-        st.write("Detecting...")
+        st.write("### Detecting...")
         classify_image(image)
 
     uploaded_file = st.file_uploader("Choose an image of cucumber...", type="jpg")
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_column_width=True)
-        st.write("")
-        st.write("Detecting...")
+        st.write("### Detecting...")
         classify_image(image)  
 
 if __name__ == "__main__":
